52f323597b8d70e7f35b1fe2702dcce739b4d2d5
madanton1994 <61697522+madanton1994@users.noreply.github.com>
2023-12-14 20:45:37 +0500

    Update variables.tf

--- modules/k8s/variables.tf
+++ modules/k8s/variables.tf
@@ -58,28 +58,5 @@ variable "k8s_cluster" {
     delete_all                       = optional(string)
     hibernate                        = optional(string)
   }))
-
-  # cidr                  = string
-  # enterprise_project_id = optional(string)
-  # tags                  = optional(map(string))
-  # vpc_subnets = map(object({
-  #   name        = string
-  #   description = optional(string)
-  #   cidr        = string
-  #   gateway_ip  = string
-  #   # vpc_id            = string
-  #   region            = optional(string)
-  #   primary_dns       = optional(string)
-  #   secondary_dns     = optional(string)
-  #   ipv6_enable       = optional(bool)
-  #   dhcp_enable       = optional(bool)
-  #   dns_list          = optional(list(string))
-  #   availability_zone = optional(string)
-  #   tags              = optional(map(string))
-  # }))
-
 }
 
-# variable "enterprise_project_id" {
-#   type = string
-# }
